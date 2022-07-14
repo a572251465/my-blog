35c5dfc99f4fa5b5a5e4eec4cd389855e91489db
lihaohao <2495041749@qq.com>
2022-07-14 22:12:42 +0800

    feat: 排序推到注释

--- 算法/二叉树/105. 从前序与中序遍历序列构造二叉树/Option 1.js
+++ 算法/二叉树/105. 从前序与中序遍历序列构造二叉树/Option 1.js
@@ -12,6 +12,21 @@
  * @return {TreeNode}
  */
 var buildTree = function (preorder, inorder) {
+  /**
+   * @description 分析
+   * 1. 特性：前序遍历中 头节点一定是在最前面
+   * 2. 特性：中序遍历中 头节点就在中间位置。而其左边以及右边就是两个子树
+   * 3. 先从前序中拿到头节点。再到中序中寻找。找到后，左边的节点就是左子树。右边的节点就是右子树
+   * 4. 这个需要构建一个新的数。头节点为root。重新递归来构建左子树 以及右子树
+   *
+   * @param preorder
+   * @param inorder
+   * @param preStart
+   * @param preEnd
+   * @param centerStart
+   * @param centerEnd
+   * @returns {TreeNode|null}
+   */
   const genTree = (
     preorder,
     inorder,
