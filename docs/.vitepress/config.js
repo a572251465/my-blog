af5f146eceb78323a2e08f50b5a2e27159450b7a
lihaohao <2495041749@qq.com>
2022-11-17 14:24:38 +0800

    feat: 添加作品

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,22 +1,33 @@
-const {sidebar, nav} = require("./sideBar")
+const { sidebar, nav } = require("./sideBar");
+const works = require("./works");
 
 export default {
   base: "/blog",
   title: "始于清风",
   description: "'帅小伙' 的知识世界 ",
+  plugins: [
+    "@vuepress/active-header-links",
+    "@vuepress/back-to-top",
+    "@vuepress/medium-zoom",
+    "@vuepress/nprogress",
+  ],
   themeConfig: {
     siteTitle: "始于清风",
     logo: "/logo.png",
-    socialLinks: [
-      { icon: "github", link: "https://github.com/a572251465" }
-    ],
+    // 启动页面丝滑滚动
+    smoothScroll: true,
+    socialLinks: [{ icon: "github", link: "https://github.com/a572251465" }],
     // 表示横向导航
     nav: [
       {
         text: "技术世界",
-        items: nav
-      }
+        items: nav,
+      },
+      {
+        text: "产品世界",
+        items: works,
+      },
     ],
-    sidebar
-  }
-}
+    sidebar,
+  },
+};
---
+++ docs/.vitepress/works.js
@@ -0,0 +1,17 @@
+const works = [
+  ["运营低代码平台", "https://github.com/a572251465/operate-drag-lowcode"],
+  ["DB表查询库", "https://github.com/a572251465/db-connect"],
+  ["生成库模板脚手架", "https://github.com/a572251465/init-tpl"],
+  ["实现主题的vite插件", "https://github.com/a572251465/postcss-theme-rc"],
+  ["公司定制组件库", "https://github.com/a572251465/vu-design-plus"],
+  ["hooks函数", "https://github.com/a572251465/w-hooks"],
+  ["热更新脚手架", "https://github.com/a572251465/hot-refresh"],
+  ["node版MySQL curd", "https://github.com/a572251465/mysql-qs-parse"],
+  ["基于node邮箱服务", "https://github.com/a572251465/micro-mail-server"],
+  ["类型判断插件", "https://github.com/a572251465/where-type"],
+  ["rollup插件", "https://github.com/a572251465/rollup-plugin-del"],
+  ["Vue3组件库", "https://github.com/a572251465/viewer-design"],
+  ["eslint部署脚手架", "https://github.com/a572251465/vite-eslint-cli"],
+];
+
+module.exports = works.map(item => ({ text: item[0], link: item[1] }));
